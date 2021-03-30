584a70004effed4ca4f11991642fbea167b4db57
VSF <viniciussafra@outlook.com>
2021-03-30 15:22:44 -0300

    P

---
+++ Procfile
@@ -0,0 +1,15 @@
+web: rails s
+mailcatcher: mailcatcher -f -v
+rake db:migrate
+
+rake db:drop db:create db:migrate db:seed utils:generate_admins utils:generate_ads utils:generate_members
+
+ou 
+
+rake dev:setup
+
+http://localhost:3000/rails/info/routes
+
+rake paperclip:refresh:thumbnails CLASS=Ad
+
+documento 74 importante
---
+++ db/seeds.rb
@@ -0,0 +1,59 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando as CATEGORIAS..."
+
+categories = ["Animais e acessórios",
+               "Esportes",
+               "Para a sua casa",
+               "Eletrônicos e celulares",
+               "Música e hobbies",
+               "Bebês e crianças",
+               "Moda e beleza",
+               "Veículos e barcos",
+               "Imóveis",
+               "Empregos e negócios"]
+
+categories.each do |category|
+  Category.friendly.find_or_create_by(description: category)
+end
+
+puts "CATEGORIAS cadastradas com sucesso!"
+
+######################################
+
+puts "Cadastrando o ADMINISTRADOR Padrão..."
+
+Admin.create!(
+  name: "Administrador Geral",
+  email: "admin@admin.com",
+  password: "123456",
+  password_confirmation: "123456",
+  role: 0
+)
+
+puts "ADMINISTRADOR cadastrado com sucesso!"
+
+###################
+
+puts "Cadastrando o MEMBRO Padrão..."
+
+member = Member.new(
+  email: "membro@membro.com",
+  password: "123456",
+  password_confirmation: "123456"
+)
+
+member.build_profile_member
+
+member.profile_member.first_name = Faker::Name.first_name
+member.profile_member.second_name = Faker::Name.last_name
+
+member.save!
+
+puts "MEMBRO cadastrado com sucesso!"

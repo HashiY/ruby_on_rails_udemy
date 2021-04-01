36ee1d4715db81542193c9052d0abdbce2d22f87
VSF <viniciussafra@outlook.com>
2021-04-01 19:26:01 -0300

    Nao usa mais o session_store.rb

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,6 +1,9 @@
 class Site::HomeController < SiteController
   
   def index
+    session[:name] = "Jackson"
+    puts ">>>>>>>>>>>> #{session[:name]}"
+    
     @categories = Category.order_by_description
     @ads = Ad.descending_order(params[:page])
     @carousel = Ad.random(3)

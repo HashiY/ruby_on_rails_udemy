2a1e1190f5d6bf39f33635650ecda8f38be3991f
VSF <viniciussafra@outlook.com>
2021-04-05 19:05:45 -0300

    Elasticsearch

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,6 +1,6 @@
 class Site::SearchController < SiteController
     def ads
-      @ads = Ad.search(params[:q], params[:page])
+      @ads = Ad.search(params[:q], fields: [:title], page: params[:page], per_page: Ad::QTT_PER_PAGE)
       @categories = Category.all
     end
 end

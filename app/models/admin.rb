90fdb582f7cb9d645af44247747d5ce1c826a2a3
VSF <viniciussafra@outlook.com>
2021-04-06 13:16:03 -0300

    Múltiplos papéis com Rolify (Pundit)

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,16 +1,16 @@
 class Admin < ActiveRecord::Base
   rolify
     # Constants
-    ROLES = {:full_access => 0, :restricted_access => 1}
+    #ROLES = {:full_access => 0, :restricted_access => 1}
 
     # Enums
-    enum role: ROLES
+    #enum role: ROLES
     
 
   # Scopes
-  scope :with_full_access, -> { where(role: ROLES[:full_access]) }
-  scope :with_restricted_access, -> { where(role: ROLES[:restricted_access]) }
-
+  #scope :with_full_access, -> { where(role: ROLES[:full_access]) }
+  #scope :with_restricted_access, -> { where(role: ROLES[:restricted_access]) }
+  scope :with_restricted_access, -> { with_role(Role::OPTIONS[1]) }
 
 
   # Include default devise modules. Others available are:
